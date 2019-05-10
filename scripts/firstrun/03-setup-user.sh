cb83ab8555399a46134ffb281d4e901cdf19cde5
Malik Mann <github@dermm.io>
2019-05-10 14:07:35 +0200

    added password to turtle user

--- scripts/firstrun/03-setup-user.sh
+++ scripts/firstrun/03-setup-user.sh
@@ -5,4 +5,5 @@
 userdel -r ubuntu || true
 userdel -r turtle || true
 
-useradd -m -G dialout,tty,adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare turtle 
+useradd -m -G dialout,tty,adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare turtle
+echo "turtle" | chpasswd turtle
