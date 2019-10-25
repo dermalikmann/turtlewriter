24e4e3fec56e425b047355c81eccc0a4c5b56b89
Sebastian Mai <sebastian.mai@ovgu.de>
2019-10-25 10:32:12 +0200

    Fix typo

--- turtlefiles/firstrun.sh
+++ turtlefiles/firstrun.sh
@@ -3,7 +3,7 @@ sudo echo "sudo permissions unlocked"
 sudo hostnamectl set-hostname $ROS_HOSTNAME
 echo "rewriting origin to use ssh"
 git remote remove origin
-git add origin git@github.com:ovgu-FINken/DrivingSwarm.git
+git remote add origin git@github.com:ovgu-FINken/DrivingSwarm.git
 git checkout uncertainty_grid
 gitman install --force
 sudo rosdep init
